75ef97a2e5f3253ed9551aa7cc27e481c6e39af2
vivid-kr <57527210+vivid-kr@users.noreply.github.com>
2019-12-01 21:04:06 +0100

    0000

---
+++ src/main/java/leetcode/No_0000_Template.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * TODO Time
+ */
+public class No_0000_Template {
+
+  public static String func(String str) {
+    return "0";
+  }
+  /**
+   * Runtime:
+   *
+   * Memory Usage:
+   *
+   * 次调试错误，然后成功。
+   *
+   * 错误：
+   *
+   * 1错：
+   *
+   * 2错：
+   *
+   * 一句话反思：
+   *
+   * 新知识：
+   */
+
+
+  public static void main(String[] args) {
+    String input = "";
+    String expect = "0";
+    String actual = func(input);
+
+    System.out.println(actual);
+    System.out.println(expect.equals(actual));
+  }
+}
+
