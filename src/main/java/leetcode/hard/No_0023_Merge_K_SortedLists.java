f7bbf4edc779bdefddf8b29b1008bd36e30a2550
vivid-kr <57527210+vivid-kr@users.noreply.github.com>
2019-12-06 14:34:30 +0100

    No_0023_Merge_K_SortedLists

---
+++ src/main/java/leetcode/hard/No_0023_Merge_K_SortedLists.java
@@ -0,0 +1,88 @@
+package leetcode.hard;
+
+/**
+ * An implementation not good enough
+ */
+public class No_0023_Merge_K_SortedLists {
+
+
+  public static class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+    }
+  }
+
+  public static ListNode mergeTwo(ListNode a, ListNode b) {
+    if (a == null) {
+      return b;
+    }
+    if (b == null) {
+      return a;
+    }
+    ListNode head;
+    if (a.val < b.val) {
+      head = a;
+      a = a.next;
+    } else {
+      head = b;
+      b = b.next;
+    }
+    ListNode p = head;
+    while (a != null && b != null) {
+      if (a.val < b.val) {
+        p.next = a;
+        a = a.next;
+      } else {
+        p.next = b;
+        b = b.next;
+      }
+      p = p.next;
+    }
+    if (a == null) {
+      p.next = b;
+    } else {
+      p.next = a;
+    }
+    return head;
+  }
+
+
+  public static ListNode mergeKLists(ListNode[] lists) {
+    if (lists == null || lists.length == 0) {
+      return null;
+    }
+    ListNode ret = lists[0];
+    for (int i = 1; i < lists.length; i++) {
+      ret = mergeTwo(ret, lists[i]);
+    }
+    return ret;
+  }
+
+  /**
+   * Runtime:
+   *
+   * Memory Usage:
+   *
+   * 次调试错误，然后成功。
+   *
+   * 错误：
+   *
+   * 1错：
+   *
+   * 2错：
+   *
+   * 一句话反思：
+   *
+   * 新知识：
+   */
+
+
+  public static void main(String[] args) {
+    System.out.println(mergeKLists(null) == null);
+  }
+}
+
